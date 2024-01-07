da1434d44ef9dab6869bf052120dadcb0b9a6190
mimifrau <67059472+mimifrau@users.noreply.github.com>
2024-01-08 01:04:25 +0300

    Create global.h

---
+++ DZ/for_this_damn_carriage_to_work/global.h
@@ -0,0 +1,29 @@
+//
+// Created by РЎРµСЂРіРµР№ РќРѕРІРёРєРѕРІ on 26.05.2023.
+//
+
+#ifndef MAIN_CPP_GLOBAL_H
+#define MAIN_CPP_GLOBAL_H
+
+#include <SFML/Graphics.hpp>
+#include <ctime>
+
+using namespace sf;
+
+const int M = 20;
+const int N = 10;
+
+int field[M][N] = {0};
+int w = 34;
+
+int figures[7][4] = {
+        1, 3, 5, 7,
+        2, 4, 5, 7,
+        3, 5, 4, 6,
+        3, 5, 4, 7,
+        2, 3, 5, 7,
+        3, 5, 7, 6,
+        2, 3, 4, 5,
+};
+
+#endif //MAIN_CPP_GLOBAL_H
